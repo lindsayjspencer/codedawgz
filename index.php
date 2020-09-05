c1b2ef964229a84a31fec0d8cd9b023af0b90f22
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-09-05 18:17:26 +1000

    Title/font

--- index.php
+++ index.php
@@ -7,12 +7,17 @@
 		<script src="/assets/vendor/jquery/jquery.min.js"></script>
 		<link type="text/css" rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
 		<link type="text/css" rel="stylesheet" href="/assets/css/codedawgz.css">
+		<link href="https://fonts.googleapis.com/css2?family=Damion&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous">
 		<script type="module" src="/assets/vendor/popperjs/popper.js"></script>
 		<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
 		<script src="/assets/js/common.js"></script>
     </head>
     <body>
-
+		<div class="nav-bar d-flex flex-row">
+			<div class="nav-container mx-auto py-3">
+				<span class="nav-title mr-auto">Codedawgz</span>
+			</div>
+		</div>
 	</body>
 </html>
