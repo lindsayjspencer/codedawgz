3a8d52e13a2f83f66a2e0c743b0a572a94c6b519
Lindsay Spencer <lindsayjspencer@gmail.com>
2020-09-05 17:31:58 +1000

    Initial index page head/body layout

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-Hello
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Codedawgz</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<script src="/assets/vendor/jquery/jquery.min.js"></script>
+		<link type="text/css" rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="/assets/css/codedawgz.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css" integrity="sha256-46qynGAkLSFpVbEBog43gvNhfrOj+BmwXdxFgVK/Kvc=" crossorigin="anonymous">
+		<script type="module" src="/assets/vendor/popperjs/popper.js"></script>
+		<script src="/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="/assets/js/common.js"></script>
+    </head>
+    <body>
+
+	</body>
+</html>
